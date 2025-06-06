e01105d6121a7285ab9890d5a69a7ef42bc3da69
Arunabh Sen <arunabhsen100705@gmail.com>
2025-06-06 20:48:25 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Intro from '@/components/intro'
-import NewsletterForm from '@/components/newsletter-form'
 import RecentPosts from '@/components/recent-posts'
 import RecentProjects from '@/components/recent-projects'
 import ToolsIUse from '@/components/tools-i-use'
